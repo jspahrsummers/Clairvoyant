84e91708b51d34a153f0fca840f5619f2c7c4bc8
Justin Spahr-Summers <justin@jspahrsummers.com>
2015-07-11 22:35:15 -0700

    Initial commit

---
+++ Clairvoyant/Clairvoyant.h
@@ -0,0 +1,19 @@
+//
+//  Clairvoyant.h
+//  Clairvoyant
+//
+//  Created by Justin Spahr-Summers on 2015-07-11.
+//  Copyright © 2015 Justin Spahr-Summers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Clairvoyant.
+FOUNDATION_EXPORT double ClairvoyantVersionNumber;
+
+//! Project version string for Clairvoyant.
+FOUNDATION_EXPORT const unsigned char ClairvoyantVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Clairvoyant/PublicHeader.h>
+
+
