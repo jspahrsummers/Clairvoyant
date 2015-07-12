020890fffb08936ab63d84a4349053a5d2a89704
Justin Spahr-Summers <justin@jspahrsummers.com>
2015-07-11 22:36:48 -0700

    Stub StoreType protocol

---
+++ Clairvoyant/Store.swift
@@ -0,0 +1,12 @@
+//
+//  Store.swift
+//  Clairvoyant
+//
+//  Created by Justin Spahr-Summers on 2015-07-11.
+//  Copyright © 2015 Justin Spahr-Summers. All rights reserved.
+//
+
+import Foundation
+
+public protocol StoreType {
+}
