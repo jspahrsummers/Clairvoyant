bac0f7dfc047b30e420f856cbcfb8b44db9d1b99
Justin Spahr-Summers <justin@jspahrsummers.com>
2015-07-25 15:45:06 -0700

    New interfaces

---
+++ Clairvoyant/Facts.md
@@ -0,0 +1,7 @@
+Entity A
+    1. Assert foo=bar
+    1. Assert fuzz=buzz
+    1. Retract foo=bar
+
+Truth:
+    - fuzz=buzz
--- Clairvoyant/Interface.swift
+++ Clairvoyant/Interface.swift
@@ -8,49 +8,78 @@
 
 import Foundation
 
-public enum FactStatus: Equatable {
-	case Unsaved
-	case Saved
-	case Retracted
-}
-
 public protocol FactType: Equatable {
-	typealias Key: Equatable
+	typealias Key: Hashable
 	typealias Value: Equatable
 
-	var status: FactStatus { get }
+	var key: Key { get }
 	var value: Value { get }
 }
 
+public protocol TimeType: Comparable {
+}
+
+public enum Event<Fact: FactType, Time: TimeType>: Equatable {
+	case Assertion(Fact, Time)
+	case Retraction(Fact, Time)
+
+	var timestamp: Time {
+		switch self {
+		case let .Assertion(_, time):
+			return time
+
+		case let .Retraction(_, time):
+			return time
+		}
+	}
+}
+
+public func == <Fact, Time>(lhs: Event<Fact, Time>, rhs: Event<Fact, Time>) -> Bool {
+	switch (lhs, rhs) {
+	case let (.Assertion(leftFact, leftTime), .Assertion(rightFact, rightTime)):
+		return leftFact == rightFact && leftTime == rightTime
+	
+	case let (.Retraction(leftFact, leftTime), .Retraction(rightFact, rightTime)):
+		return leftFact == rightFact && leftTime == rightTime
+	
+	default:
+		return false
+	}
+}
+
 public protocol EntityType: Equatable {
-	typealias Identifier: Equatable
+	typealias Identifier: Hashable
+	typealias Fact: FactType
+	typealias Time: TimeType
 
-	var identifier: Identifier { get }
+	var facts: AnyForwardCollection<Fact> { get }
+	func factsAssertedInTimeInterval(interval: HalfOpenInterval<Time>) -> AnyForwardCollection<Fact>
 
-	subscript<F: FactType>(key: F.Key) -> F.Value? { get }
+	var history: AnyForwardCollection<Event<Fact, Time>> { get }
+	func historyInTimeInterval(interval: HalfOpenInterval<Time>) -> AnyForwardCollection<Event<Fact, Time>>
 
-	mutating func addFact<F: FactType>(fact: F) throws
-	mutating func retractFact<F: FactType>(fact: F)
+	subscript(key: Fact.Key) -> Fact? { get }
 }
 
-public protocol GenerationType {
+public protocol TransactionType {
 	typealias Entity: EntityType
-	typealias Index: ForwardIndexType
 
-	subscript(entityID: Entity.Identifier) -> Entity? { get }
-}
+	var timestamp: Entity.Time { get }
 
-public protocol TransactionType: GenerationType {
-	func insertEntityWithFacts(facts: [Entity.Fact.Key: Entity.Fact.Value]) throws -> Entity
+	var entities: AnyForwardCollection<Entity> { get }
+	func entitiesCreatedInTimeInterval(interval: HalfOpenInterval<Entity.Time>) -> AnyForwardCollection<Entity>
 
-	subscript(entityID: Entity.Identifier) -> Entity? { get set }
+	mutating func createEntity(identifier: Entity.Identifier) throws -> Entity
+	mutating func assertFact(fact: Entity.Fact, forEntityWithIdentifier: Entity.Identifier) throws
+	mutating func retractFact(fact: Entity.Fact, forEntityWithIdentifier: Entity.Identifier) throws
+
+	subscript(identifier: Entity.Identifier) -> Entity? { get }
 }
 
 public protocol StoreType {
 	typealias Transaction: TransactionType
 
-	func factsAboutEntity<T>(entity: Transaction.Entity, asOfTransactionID: Transaction.Identifier? = nil) throws -> [Fact<T>]
-
 	func openTransaction() -> Transaction
-	func saveTransaction(transaction: Transaction) throws -> Transaction.Identifier
+
+	mutating func saveTransaction(transaction: Transaction) throws
 }
